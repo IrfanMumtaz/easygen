25aa850b5b706870b4bb07e4a18d00c36a8f8507
irfan mumtaz <im.irfanmumtaz21@gmail.com>
2023-12-06 10:06:25 +0400

    added createdAt, updatedAt in schema

--- backend/schemas/User.ts
+++ backend/schemas/User.ts
@@ -1,4 +1,5 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { now } from "mongoose";
 
 @Schema()
 export class User {
@@ -11,6 +12,12 @@ export class User {
 
     @Prop({ required: true })
     password: string;
+
+    @Prop({default: now()})
+    createdAt: Date;
+
+    @Prop({default: now()})
+    updatedAt: Date;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
